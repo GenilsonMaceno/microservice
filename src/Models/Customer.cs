4959871e713883578b3689b05606bc0d298cffd9
Genilson Maceno <genilson_de_maceno@hotmail.com>
2025-01-29 14:41:05 -0300

    create class customer

--- src/Models/Customer.cs
+++ src/Models/Customer.cs
@@ -1,16 +1,27 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace src.Models
 {
+    [Table("customer", Schema ="dbo")]
     public class Customer
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("customer_id")]
         public int CustomerId { get; set; }
+
+        [Column("customer_name")]
         public string CustomerName { get; set;}
 
+        [Column("mobile_id")]
         public string MobileNumber  { get; set; }
+
+        [Column("email")]
         public string Email { get; set;}
     }
 }
