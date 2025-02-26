8321eb8e81ddde5e9800fdde0d1f798ad188d35e
Genilson Maceno <genilson_de_maceno@hotmail.com>
2025-02-26 19:02:52 -0300

    added service controllers and other config about vs code

--- src/Program.cs
+++ src/Program.cs
@@ -5,8 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// add services to the controllers
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 /* add database context dependency Injection */
 
