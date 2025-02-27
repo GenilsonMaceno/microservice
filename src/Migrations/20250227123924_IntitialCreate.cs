8b32f8d17f7e0aee07a2ee6b6541ab481d45f715
Genilson Maceno <genilson_de_maceno@hotmail.com>
2025-02-27 09:44:59 -0300

    Added folder bin and obj in gitignore and migrations

---
+++ src/Migrations/20250227123924_IntitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomerWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "customer",
+                schema: "dbo",
+                columns: table => new
+                {
+                    customer_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    customer_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    mobile_id = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customer", x => x.customer_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "customer",
+                schema: "dbo");
+        }
+    }
+}
